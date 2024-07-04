8cd6e812512314a1c9795a1fd49d700c5c6d23c8
Wisdom <wisdomokwen@gmail.com>
2024-07-04 16:29:21 -0400

    Setup dummy backend

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,43 @@
-from fastapi import FastAPI, HTTPException, Depends
-from pydantic import BaseModel
-from typing import List, Annotated
+"""Entrypoint of backend API exposing the FastAPI `app` to be served by an application server such as uvicorn."""
 
-app = FastAPI()
+from pathlib import Path
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.middleware.gzip import GZipMiddleware
 
-class Post(BaseModel):
-    id: int
-    description: str
+description = """
+Welcome to the **My Fun App** RESTful Application Programming Interface.
+"""
+
+# Metadata to improve the usefulness of OpenAPI Docs /docs API Explorer
+app = FastAPI(
+    title="My Fun App",
+    version="0.0.1",
+    description=description,
+    openapi_tags=[],
+)
+
+# Use GZip middleware for compressing HTML responses over the network
+app.add_middleware(GZipMiddleware)
+
+# Plugging in each of the router APIs
+feature_apis = []
+
+for feature_api in feature_apis:
+    app.include_router(feature_api.api)
+
+# Static file mount used for serving React front-end in production, as well as static assets
+# app.mount("/", static_files.StaticFileMiddleware(directory=Path("./static")))
 
 
+# Add application-wide exception handling middleware for commonly encountered API Exceptions
+# @app.exception_handler(UserPermissionException)
+# def permission_exception_handler(request: Request, e: UserPermissionException):
+#     return JSONResponse(status_code=403, content={"message": str(e)})
 
-@app.get("/")
-def read_root():
-    return {"Hello there!": "Welcome"}
+
+# @app.exception_handler(ResourceNotFoundException)
+# def resource_not_found_exception_handler(
+#     request: Request, e: ResourceNotFoundException
+# ):
+#     return JSONResponse(status_code=404, content={"message": str(e)})
