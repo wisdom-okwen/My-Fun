fe0673f64a1a57625d5037ae15a4db9f8bc1a480
Wisdom <wisdomokwen@gmail.com>
2024-07-04 09:35:47 -0700

    Render Landing page

--- frontend/my-fun/src/App.tsx
+++ frontend/my-fun/src/App.tsx
@@ -1,12 +1,18 @@
 import './App.css';
 import React from 'react';
+import { LandingPage } from './components/LandingPage';
 
 interface AppProps{
     message: string;
 }
 
 const App: React.FC<AppProps> = ({message}) => {
-    return <div>{message}</div>
+    return (
+        <>
+            <div>{message}</div>
+            <LandingPage />
+        </>
+    );
 }
 
 export default App;
