f570098079d14848409fcc6d072801ec60b0ba39
Wisdom <wisdomokwen@gmail.com>
2024-07-06 00:43:25 -0400

    Navigate to login page when profile avatar clicked

--- frontend/my-fun/src/components/Profile/Profile.tsx
+++ frontend/my-fun/src/components/Profile/Profile.tsx
@@ -1,12 +1,19 @@
 import { Avatar } from "@mui/material";
 import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../auth/AuthContext";
 
 const Profile: React.FC = () => {
     const currentUser = useContext(UserContext);
+    const navigate = useNavigate();
+
+    const handleAvatarClick = () => {
+        navigate("/login");
+    };
+
     return (
         <>
-            <Avatar alt="Remy Sharp" src="" />
+            <Avatar onClick={handleAvatarClick} alt="Remy Sharp" src="" />
         </>
     );
 };
