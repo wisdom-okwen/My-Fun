56ec4f5b8e4105cb46a322a0fd18a60a3105826a
Wisdom <wisdomokwen@gmail.com>
2024-07-06 01:13:18 -0400

    Setup services for frontend, add user service

---
+++ frontend/my-fun/src/services/UserService.ts
@@ -0,0 +1,33 @@
+import axios, { Axios } from 'axios';
+import { User } from '../models/User.model';
+
+interface UserService {
+  getUser(username: string): Promise<User | null>;
+  updateUser(username: string, userData: Partial<User>): Promise<User | null>;
+  createUser(userData: Partial<User>): Promise<User |null>;
+};
+
+class UserServiceImpl implements UserService {
+    private baseUrl: string;
+    constructor(protected axios: Axios, baseUrl: string) {
+        this.baseUrl = baseUrl;
+    };
+
+    getUser(username: string): Promise<User | null> {
+        throw new Error('Method not implemented.');
+    }
+    updateUser(username: string, userData: Partial<User>): Promise<User | null> {
+        throw new Error('Method not implemented.');
+    }
+    createUser(userData: Partial<User>): Promise<User | null> {
+        return axios.post<User>(`${this.baseUrl}/user`, userData)
+            .then(response => response.data)
+            .catch(error => {
+                console.error(error);
+                return null;
+            });
+    };
+    
+};
+
+export { UserServiceImpl }
