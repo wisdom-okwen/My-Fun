134d909bbf925ddf593060121976424aa7795c44
Wisdom Okwen <wokwen@unc.edu>
2024-12-23 23:59:23 -0500

    Improve style, add more dummy data

--- frontend/my-fun/src/components/PostForm/PostForm.tsx
+++ frontend/my-fun/src/components/PostForm/PostForm.tsx
@@ -14,27 +14,27 @@ const postService = new PostServiceImpl(postURL);
 
 const PostForm: React.FC<PostFormProps> = ({ open, onClose, onPostCreated }) => {
 
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const formData = new FormData(event.currentTarget);
-        const imageUrl = formData.get("title") as string; 
-        const description = formData.get("description") as string;
-        const time = new Date().toLocaleTimeString();
-        const date = new Date().toLocaleDateString();
-        const post: Post = {
-            description: description,
-            author_id: 1,
-            state: 0,
-            image_url: imageUrl,
-            num_likes: 0,
-            num_shares: 0,
-            num_comments: 0,
-            time: time,
-            date: date
-        }
-        await postService.createPost(post)
-        onPostCreated();
-        onClose();
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      const formData = new FormData(event.currentTarget);
+      const imageUrl = formData.get("imgURL") as string; 
+      const description = formData.get("description") as string;
+      const time = new Date().toLocaleTimeString();
+      const date = new Date().toLocaleDateString();
+      const post: Post = {
+          description: description,
+          author_id: 1,
+          state: 0,
+          image_url: imageUrl,
+          num_likes: 0,
+          num_shares: 0,
+          num_comments: 0,
+          time: time,
+          date: date
+      }
+    await postService.createPost(post);
+    onPostCreated();
+    onClose();
     };
 
   return (
@@ -58,7 +58,7 @@ const PostForm: React.FC<PostFormProps> = ({ open, onClose, onPostCreated }) => 
         </Typography>
         <form onSubmit={handleSubmit}>
           <TextField
-            name="title"
+            name="imgURL"
             label="Image URL"
             variant="outlined"
             fullWidth
@@ -98,7 +98,7 @@ const PostForm: React.FC<PostFormProps> = ({ open, onClose, onPostCreated }) => 
             <Button onClick={onClose} variant="outlined">
               Cancel
             </Button>
-            <Button type="submit" variant="contained">
+            <Button type="submit" variant="contained" sx={{ backgroundColor: '#0056b3' }}>
               Submit
             </Button>
           </Box>
