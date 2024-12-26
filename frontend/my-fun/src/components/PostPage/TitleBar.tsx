07cc79657503848a92b9e2d3ce9aba58631cec66
Wisdom Okwen <wokwen@unc.edu>
2024-12-25 21:55:24 -0500

    Add gear icon

--- frontend/my-fun/src/components/PostPage/TitleBar.tsx
+++ frontend/my-fun/src/components/PostPage/TitleBar.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import { LandingPage } from "../LandingPage/LandingPage";
+import SettingsIcon from '@mui/icons-material/Settings';
+import './TitleBar.css';
 
 const TitleBar: React.FC = () => {
     return (
         <div className="title-bar">
             <LandingPage />
+            <div className="settings-icon">
+                <SettingsIcon
+                    sx={{
+                        color: '#0056b3',
+                        cursor: 'pointer',
+                        transform: 'rotate(-30deg)',
+                        transition: 'transform 0.2s, color 0.2s',
+                    }}
+                />
+            </div>
         </div>
 
     );
