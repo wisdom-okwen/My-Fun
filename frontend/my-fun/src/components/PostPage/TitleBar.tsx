0f6d1f3f83e6519866fddb096b14e8866257c6e3
Wisdom Okwen <65198244+wisdom-okwen@users.noreply.github.com>
2024-12-27 14:28:25 -0500

    Add settings gear icon and shuffle published posts (#17)
    * Implement like functionality
    
    * Add gear icon
    
    * Add gear icon and shuffle posts
    
    ---------
    
    Co-authored-by: Wisdom Okwen <wokwen@unc.edu>

--- frontend/my-fun/src/components/PostPage/TitleBar.tsx
+++ frontend/my-fun/src/components/PostPage/TitleBar.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { LandingPage } from "../LandingPage/LandingPage";
+import SettingsIcon from '@mui/icons-material/Settings';
+import './TitleBar.css';
 
 const TitleBar: React.FC = () => {
     return (
         <div className="title-bar">
-            <LandingPage />
+            <div className="title-center">
+                <LandingPage />
+            </div>
+            <div className="settings-icon">
+                <SettingsIcon
+                    sx={{
+                        color: '#0056b3',
+                        cursor: 'pointer',
+                        transform: 'rotate(-30deg)',
+                        transition: 'transform 0.2s, color 0.2s',
+                    }}
+                />
+            </div>
         </div>
-
     );
 }
 
